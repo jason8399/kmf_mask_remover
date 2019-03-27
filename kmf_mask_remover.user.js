39934c05c479af5b22868ae5735e39d379984973
JasonPan <jason8399@gmail.com>
2019-03-27 21:56:12 +0800

    Upgraded to 0.1.2
    - Add null return checking.
    - Add interval when selecting practice-container and shield-box.

--- kmf_mask_remover.user.js
+++ kmf_mask_remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         kmf_mask_remover
 // @namespace    https://github.com/jason8399
-// @version      0.1.1
+// @version      0.1.2
 // @description  Remove the blurred mask added by the policy of kmf to restrict globle user.
 // @author       JasonPan
 // @match        https://toefl.kmf.com/*/ets/*
@@ -12,14 +12,19 @@
 (function() {
     'use strict';
 
-    document.querySelector('.shield-box').remove();
-    var practice_container = document.querySelector('.practice-container');
+    var shield_box;
+    var practice_container;
     var class_str;
     var interval = setInterval(() => {
+        shield_box = document.querySelector('.shield-box');
+        if (shield_box !== null) {
+            shield_box.remove();
+        } 
+        practice_container = document.querySelector('.practice-container');
         class_str = practice_container.getAttribute('class');
         if (class_str.indexOf('blur') != -1){
             practice_container.setAttribute('class', class_str.replace(' blur', ''));
             clearInterval(interval);
         }
-    }, 500);
+    }, 750);
 })();
